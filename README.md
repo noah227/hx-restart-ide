85574cd01a5a166db784d6ca7eed9e66d367471d
noah227 <noah227@foxmail.com>
2024-03-14 16:38:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+﻿> 一款单纯的IDE重启插件，简单粗暴的taskkill
+
+## 使用
+
+`文件 > 重启IDE`
+
+![](https://mp-77dc03ae-7084-429e-8b0f-4d540ae4a430.cdn.bspapp.com/images/hx-restart-ide-1.jpg)
+
+重启前的提示可以在`工具 > 设置 > 插件配置 > IDE重启`进行勾选配置，**插件安装时默认勾选**
+
+![](https://mp-77dc03ae-7084-429e-8b0f-4d540ae4a430.cdn.bspapp.com/images/hx-restart-ide-2.jpg)
---
+++ extension.js
@@ -0,0 +1,64 @@
+var hx = require("hbuilderx");
+const {
+	exec
+} = require("child_process")
+
+
+const getConfiguration = () => {
+	const pkg = require("./package.json")
+	return hx.workspace.getConfiguration(pkg.id)
+}
+
+const configKey = "promptBeforeRestart"
+/**
+ * 检测是否需要在重启前提示
+ */
+const needPrompt = () => { 
+	return getConfiguration().get(configKey)
+}
+
+//该方法将在插件激活的时候调用
+function activate(context) {
+	let disposable = hx.commands.registerCommand('extension.restartIde', () => {
+		const action = () => {
+			// 销毁进程
+			exec("taskkill /f /im hbuilderx.exe", (err, stdout, stderr) => {
+				if (err) hx.window.showErrorMessage(err)
+				else {
+					const appRoot = hx.env.appRoot
+					// 重启进程
+					exec(`hbuilderx.exe`, {cwd: appRoot}, (err, stdout, stderr) => {
+						hx.window.showErrorMessage(err)
+					})
+				}
+			})
+		}
+		const _ = needPrompt()
+		if (_) {
+			hx.window.showMessageBox({
+				type: 'question',
+				title: '提示',
+				text: '重启IDE？',
+				buttons: ['重启', '重启并不再提示', '取消'],
+				defaultButton: "重启"
+			}).then(button => {
+				if (button === "重启") action()
+				else if(button === "重启并不再提示") {
+					getConfiguration().update(configKey, false)
+					action()
+				}
+				else {}
+			})
+		} else action()
+	});
+	//订阅销毁钩子，插件禁用的时候，自动注销该command。
+	context.subscriptions.push(disposable);
+}
+//该方法将在插件禁用的时候调用（目前是在插件卸载的时候触发）
+function deactivate() {
+
+}
+module.exports = {
+	activate,
+	deactivate
+}
